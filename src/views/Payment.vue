8f7efe38cad8508884f9512c52eeda7b7e3df724
plasticgrammer <plasticgrammer@gmail.com>
2024-11-11 00:01:07 +0900

    プラン選択 UI調整

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -43,16 +43,17 @@
               <!-- 現在のプランバッジ -->
               <v-chip
                 v-if="currentPlan?.planId === plan.planId"
-                color="primary"
+                :color="formState.selectedPlan === plan.planId ? 'white' : 'info'"
                 class="current-plan-badge font-weight-bold"
                 size="small"
                 label
               >
                 現在のプラン
               </v-chip>
+              <div v-else class="mb-8"></div>
 
               <!-- 既存のカード内容 -->
-              <v-card-title class="text-center pt-6">
+              <v-card-title class="text-center pt-2">
                 <div class="text-h6 font-weight-bold">{{ plan.name }}</div>
               </v-card-title>
 
