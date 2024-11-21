48ba962c543b5186bf8bd09b3cfa7ecbf4150d77
plasticgrammer <plasticgrammer@gmail.com>
2024-11-22 08:51:16 +0900

    不具合修正

--- src/views/Billing.vue
+++ src/views/Billing.vue
@@ -148,7 +148,7 @@ const fetchInvoices = async (customerId) => {
 }
 
 const formatDate = (timestamp) => {
-  const date = new Date(timestamp * 1000)  // UnixタイムスタンプをDateオブジェクトに変換
+  const date = new Date(timestamp * 1000)
   const options = { year: 'numeric', month: '2-digit', day: '2-digit' }
   return date.toLocaleDateString('ja-JP', options)
 }
